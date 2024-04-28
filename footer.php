4682556dd50b95ef4ed3ebcb4ee4d637257798bc
JessicaQC <79950970+JessicaQC@users.noreply.github.com>
2024-04-28 21:56:04 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,19 @@
   <footer class="container-fluid text-center">
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
-          <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKINS</span></a>
+        <div class="col-md-6 widgetleft">
+          <!-- <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKINS</span></a> -->
+          <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
+
 
         </div>
         <div class="col-md-6">
-          <p>Terms and conditions</p>
+        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+endif; ?>
+
+
+          <!-- <p>Terms and conditions</p> -->
 
         </div>
       </div>
